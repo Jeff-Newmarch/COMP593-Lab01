77b9316b70665cd8c3b434dd51aa7fd1d519c50a
Jeff-Newmarch <Jeff.Newmarch@flemingcollege.ca>
2023-01-18 13:55:46 -0500

    Modified both files in repo clone
    Deleted a print from script1.py
    Added a print to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 print("There should be one-- and preferably only one --obvious way to do it.")
 # This script is awesome
 print("Although that way may not be obvious at first unless you're Dutch.")
+print("Hey Guido!")
